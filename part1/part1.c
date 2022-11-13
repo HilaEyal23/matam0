b19ff78c9ce592544a3b639c13ff6e7e469bbb4c
HilaEyal23 <hilaeyal04@gmail.com>
2022-11-13 10:02:43 +0200

    Added mtm_buggy.c

--- part1/part1.c
+++ part1/part1.c
@@ -5,30 +5,35 @@
 #define NOT_POWER_OF_TWO -2000
 
 enum {false, true};
+//===============================================================================
 
-int calculate_exponent(int number);
-int is_int(char str[]);
+/** Calculates the exponent of 2 which equals the number. Returns negetive number if the number isn't a power of two **/
+int calculateExponent(int number);
+
+/** Return true iff str represents int **/
+int isInt(char str[]);
+
+//===============================================================================
 
 int main()
 {
   printf("Enter size of input:\n");
-  int input_size = {0};
+  int inputSize = {0};
   char str[50];
   scanf("%s", str);
-  input_size = atoi(str);
-  if(!is_int(str) || input_size <= 0)
+  inputSize = atoi(str);
+  if(!isInt(str) || inputSize <= 0)
   {
     printf("Invalid size\n");
     return 0;
   }
-  input_size = atoi(str);
+  inputSize = atoi(str);
 
   printf("Enter numbers:\n");
-  int numbers_arr[input_size];
-  int i = 0;
-  for(i = 0; i < input_size; i++)
+  int numbersArr[inputSize];
+  for(int i = 0; i < inputSize; i++)
     {
-      if(!scanf("%d", &numbers_arr[i]))
+      if(!scanf("%d", &numbersArr[i]))
       {
         printf("Invalid number\n");
         return 0;
@@ -36,12 +41,12 @@ int main()
     }
   int sum = 0;
   int currPower = 0;
-  for(i = 0; i < input_size; i++)
+  for(int i = 0; i < inputSize; i++)
     {
-      currPower = calculate_exponent(numbers_arr[i]);
+      currPower = calculateExponent(numbersArr[i]);
       if(currPower >= 0)
       {
-        printf("The number %d is a power of 2: %d = 2^%d\n", numbers_arr[i], numbers_arr[i], currPower);
+        printf("The number %d is a power of 2: %d = 2^%d\n", numbersArr[i], numbersArr[i], currPower);
         sum += currPower;
       }
     }
@@ -52,22 +57,19 @@ printf("Total exponent sum is %d\n", sum);
 
 
 
-int calculate_exponent(int number)
+int calculateExponent(int number)
 {
-  //if(number == 2)
-    //return 1;
   if(number == 1)
     return 0;
   if(number < 2 || (number % 2 != 0))
     return NOT_POWER_OF_TWO;
-  return calculate_exponent(number/2) + 1;
+  return calculateExponent(number/2) + 1;
 }
 
-int is_int(char str[])
+int isInt(char str[])
 {
   int len = strlen(str);
-  int i = 0;
-  for(i = 0; i < len; i++)
+  for(int i = 0; i < len; i++)
     {
       if(!('1' <= str[i] && str[i] <= '9'))
       {
