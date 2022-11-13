c9f5963f2d87ca3341c8176537342cdbc6407011
HilaEyal23 <hilaeyal04@gmail.com>
2022-11-13 10:00:26 +0200

    first commit

---
+++ part1.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <string.h> // for strlen()
+#include <stdlib.h> // for atoi()
+
+#define NOT_POWER_OF_TWO -2000
+
+enum {false, true};
+//===============================================================================
+
+/** Calculates the exponent of 2 which equals the number. Returns negetive number if the number isn't a power of two **/
+int calculateExponent(int number);
+
+/** Return true iff str represents int **/
+int isInt(char str[]);
+
+//===============================================================================
+
+int main()
+{
+  printf("Enter size of input:\n");
+  int inputSize = {0};
+  char str[50];
+  scanf("%s", str);
+  inputSize = atoi(str);
+  if(!isInt(str) || inputSize <= 0)
+  {
+    printf("Invalid size\n");
+    return 0;
+  }
+  inputSize = atoi(str);
+
+  printf("Enter numbers:\n");
+  int numbersArr[inputSize];
+  for(int i = 0; i < inputSize; i++)
+    {
+      if(!scanf("%d", &numbersArr[i]))
+      {
+        printf("Invalid number\n");
+        return 0;
+      }
+    }
+  int sum = 0;
+  int currPower = 0;
+  for(int i = 0; i < inputSize; i++)
+    {
+      currPower = calculateExponent(numbersArr[i]);
+      if(currPower >= 0)
+      {
+        printf("The number %d is a power of 2: %d = 2^%d\n", numbersArr[i], numbersArr[i], currPower);
+        sum += currPower;
+      }
+    }
+printf("Total exponent sum is %d\n", sum);
+
+  return 0;
+}
+
+
+
+int calculateExponent(int number)
+{
+  if(number == 1)
+    return 0;
+  if(number < 2 || (number % 2 != 0))
+    return NOT_POWER_OF_TWO;
+  return calculateExponent(number/2) + 1;
+}
+
+int isInt(char str[])
+{
+  int len = strlen(str);
+  for(int i = 0; i < len; i++)
+    {
+      if(!('1' <= str[i] && str[i] <= '9'))
+      {
+        //if(!(str[i] == '\n' || str[i] == EOF || str[i] == '\0')) // The least character is '\n' when using the tesminal
+          return false;
+      }
+    }
+  return true;
+}
+
